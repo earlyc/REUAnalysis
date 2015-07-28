5133d346a06bf998f89931cb844489db5659b77a
earlyc <early.chris@gmail.com>
2015-07-28 11:15:28 -0500

    Now plots data for each algorithm in a separate pdf

--- plot.R
+++ plot.R
@@ -1,11 +1,33 @@
 library(car)
 
-data = read.csv("ResultLogs/RFTest1P_LOG_Parsed.txt")
-
+data = read.csv("../ResultLogs/RFTest1P_LOG_Parsed.txt")
 newboost = data[1:162,]
-
 boostsub <- newboost[ (grep("R_50",newboost$ModelFile)),]
-
-#scatterplot(boostsub$Accuracy ~ boostsub$Time | boostsub$ModelFile, data=boostsub[c(3,6)])
-
+pdf("plot_1P_RF.pdf")
 scatterplot(boostsub$Accuracy ~ boostsub$Time, boostsub[c(6,3)])
+dev.off()
+
+
+data = read.csv("../ResultLogs/BATest1P_LOG_Parsed.txt")
+newboost = data[1:162,]
+boostsub <- newboost[ (grep("R_50",newboost$ModelFile)),]
+pdf("plot_1P_BA.pdf")
+scatterplot(boostsub$Accuracy ~ boostsub$Time, boostsub[c(6,3)])
+dev.off()
+
+
+data = read.csv("../ResultLogs/BOTest1P_LOG_Parsed.txt")
+newboost = data[1:162,]
+boostsub <- newboost[ (grep("R_50",newboost$ModelFile)),]
+#scatterplot(boostsub$Accuracy ~ boostsub$Time | boostsub$ModelFile, data=boostsub[c(3,6)])
+pdf("plot_1P_BO.pdf")
+scatterplot(boostsub$Accuracy ~ boostsub$Time, boostsub[c(6,3)])
+dev.off()
+
+
+data = read.csv("../ResultLogs/SVTest1P_LOG_Parsed.txt")
+newboost = data[1:162,]
+boostsub <- newboost[ (grep("R_50",newboost$ModelFile)),]
+pdf("plot_1P_SV.pdf")
+scatterplot(boostsub$Accuracy ~ boostsub$Time, boostsub[c(6,3)])
+dev.off()
